482d0009dd5eb6cc0f86d49a7419ea12f343cb35
Saint <yoyothai00@gmail.com>
2016-05-27 08:50:38 +0700

    init

---
+++ app/controllers/home.js
@@ -0,0 +1,39 @@
+var express = require('express'),
+  router = express.Router();
+var sys = require('sys')
+var exec = require('child_process').exec;
+var q = require('q');
+
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+
+module.exports = function (app) {
+  app.use('/', router);
+};
+
+router.post('/', function (req, res, next) {
+  var data = req.body.data;
+  var defered = q.defer();
+
+    defered.promise.then(function() {
+        var d = q.defer();
+    	exec('echo' + data + ' > input.txt', function(error, stdout, stderr){
+    		d.resolve();
+  		});
+        return d.promise;
+    })
+    .then(function(){
+    	var d = q.defer();
+    	exec('hadoop fs -put -f input.txt', function(error, stdout, stderr){
+    		d.resolve();
+  		});
+        return d.promise;
+    })
+    .then(function(){
+    	var d = q.defer();
+    	exec('pig -x mapreduce search.pig', function(error, stdout, stderr){
+    		d.resolve();
+  		});
+        return d.promise;
+    });
+  	res.send(200);
+});
